450ca5cebd206a7f30acbfa9b0fced6910927682
libbyschuknight <libbysk@gmail.com>
2016-04-03 17:26:05 +1200

    Start of rake file.

---
+++ lib/tasks/rake_csv.rb
@@ -0,0 +1,16 @@
+Stance.create(
+  description: "lakhsdfhkaskldf",
+  image_file_name, image_content_type, image_file_size, image_updated_at, japanese_name, english_name
+
+
+)
+
+
+EmpAttachment.create(picture: File.open("#{Rails.root}/features/support/rails.png"))
+
+
+/Users/SchuKnight/Projects/libs_seido_guide/public/system/stances/images/000/000/001/original/IMG_7697.JPG
+
+public/system/stances/images/000/000/001/original/IMG_7697.JPG
+
+Stance.create(description: "aksdfkhshkdf", created_at: Time.now, updated_at: Time.now, image: File.open("#{Rails.root}/public/system/stances/images/000/000/001/original/IMG_7697.JPG"), japanese_name: "DDJJDDKD", english_name: "aksdjfasjdfljs")
