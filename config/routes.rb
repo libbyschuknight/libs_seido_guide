21f1c86d873f39cbbcad7c6d6a06da629b2f8df0
libbyschuknight <libbysk@gmail.com>
2016-04-16 17:23:12 +1200

    Add static home page for natural weapons.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :stances
   get 'home/index'
+  get "/:page" => "static_pages#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
