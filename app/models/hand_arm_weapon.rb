01d47a727f6ace68b9228c7a1b09fe5deeb01932
libbyschuknight <libbysk@gmail.com>
2016-06-05 10:26:59 +1200

    Add content for resource feet lead weapons.

--- app/models/hand_arm_weapon.rb
+++ app/models/hand_arm_weapon.rb
@@ -1,10 +1,10 @@
 class HandArmWeapon < ActiveRecord::Base
-    has_attached_file :image, styles: { small: "100x100", med: "200x200", large: "400x400" }
+  has_attached_file :image, styles: { small: "100x100", med: "200x200", large: "400x400" }
 
-    validates :japanese_name, presence: true
-    validates :english_name, presence: true
-    validates :description, presence: true
+  validates :japanese_name, presence: true
+  validates :english_name, presence: true
+  validates :description, presence: true
 
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-    validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
 end
