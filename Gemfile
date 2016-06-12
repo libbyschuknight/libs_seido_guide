d8b6cefaa88774b19c41dff848c1b2c1a070a8e4
libbyschuknight <libbysk@gmail.com>
2016-06-12 16:06:18 +1200

    Add specs for model.

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'puma'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-  gem 'pry'
+  gem 'pry-byebug'
   gem 'rspec-rails'
   gem 'capybara'
   gem 'selenium-webdriver'
--- spec/models/stance_spec.rb
+++ spec/models/stance_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Stance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "valid params" do
+
+    let(:stance) { FactoryGirl.build(:stance) }
+
+    # change all specs to this way? (figured out after doing invalid)
+    it "stance is valid" do
+      expect(stance).to be_valid
+    end
+
+    it "has a japanese name" do
+      expect(stance.japanese_name).to eq("Kiba Dachi")
+    end
+
+
+    it "has an english name" do
+      expect(stance.english_name).to eq("Straddle Stance")
+    end
+
+    it "has a description name" do
+      expect(stance.description).to be_present
+    end
+
+    describe "image" do
+
+      it { should have_attached_file(:image) }
+
+      it { should validate_attachment_content_type(:image).
+        allowing('image/png', 'image/gif').
+        rejecting('text/plain', 'text/xml') }
+
+      it { should validate_attachment_size(:image).
+        less_than(1.megabytes) }
+
+      xit "should be valid if no image is attached" do
+        # FIX: at moment passes if have image as well
+        stance = FactoryGirl.build(:stance, image: nil)
+        expect(stance).to be_valid
+      end
+
+    end
+
+  end
+
+  context "invalid params" do
+
+    it "is invalid without japanese name" do
+      expect(FactoryGirl.build(:stance, japanese_name: nil)).to be_invalid
+    end
+
+    it "is invalid without englsih name" do
+      expect(FactoryGirl.build(:stance, english_name: nil)).to be_invalid
+    end
+
+    it "is invalid without description name" do
+      expect(FactoryGirl.build(:stance, description: nil)).to be_invalid
+    end
+
+  end
+
 end
