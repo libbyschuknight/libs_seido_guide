b910e842a1c15b415ca51a7e24ce790c9c388bdd
libbyschuknight <libbysk@gmail.com>
2017-07-01 15:51:35 +1200

    Add user

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord
+  #code
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606050812) do
+ActiveRecord::Schema.define(version: 20170701034819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,13 @@ ActiveRecord::Schema.define(version: 20160606050812) do
     t.datetime "image_updated_at"
     t.string   "japanese_name"
     t.string   "english_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
