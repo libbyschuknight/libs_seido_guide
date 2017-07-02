24b2546f3427120b6337d84c7d8a205809811bcd
libbyschuknight <libbysk@gmail.com>
2017-07-02 19:18:12 +1200

    Add update edit partials actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,29 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      flash[:success] = "Welcome to the Lib's Seido Site Blog #{@user.username}"
+      redirect_to root_path
     else
       render "new"
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
